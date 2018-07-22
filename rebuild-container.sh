512600be6a0c0772041f8785c74e1ed972fdf14e
Cameron Ring <cameron@cs.stanford.edu>
2018-07-21 19:41:44 -0700

    add zwave front door lock (schlage BE469)

--- rebuild-container.sh
+++ rebuild-container.sh
@@ -20,4 +20,12 @@ fi
 
 docker rm -f home-assistant >&/dev/null
 
-docker run -d --name home-assistant --net=host -v /volume1/docker/home-assistant:/config --restart=always homeassistant/home-assistant:latest
+docker run \
+  -d \
+  --name home-assistant \
+  --net=host \
+  -v /volume1/docker/home-assistant:/config \
+  --restart=always \
+  --device=/dev/ttyACM0 \
+  --privileged=true \
+  homeassistant/home-assistant:latest
